225ccd994a430bcd9a36e360c57f1474bcdde441
Jean Machuca <correojean@gmail.com>
2026-09-11 03:22:20 +0000

    feat: MIT license migration (LGPLv3 -> MIT, v2.6.0)

--- src/ts/org.qcobjects.controllers.list.ts
+++ src/ts/org.qcobjects.controllers.list.ts
@@ -5,17 +5,8 @@
  * Author: Jean Machuca <correojean@gmail.com>
  *
  * Cross Browser Javascript Framework for MVC Patterns
- * QuickCorp/QCObjects is licensed under the
- * GNU Lesser General Public License v3.0
- * [LICENSE] (https://github.com/QuickCorp/QCObjects/blob/master/LICENSE.txt)
- *
- * Permissions of this copyleft license are conditioned on making available
- * complete source code of licensed works and modifications under the same
- * license or the GNU GPLv3. Copyright and license notices must be preserved.
- * Contributors provide an express grant of patent rights. However, a larger
- * work using the licensed work through interfaces provided by the licensed
- * work may be distributed under different terms and without source code for
- * the larger work.
+ * QCObjects is licensed under the MIT License
+ * [LICENSE] (https://github.com/QCObjects/qcobjects-sdk/blob/main/LICENSE.txt)
  *
  * Copyright (C) 2015 Jean Machuca,<correojean@gmail.com>
  *
@@ -42,7 +33,7 @@ export class ListController extends Controller {
   _componentRoot: HTMLElement | undefined;
 
   constructor({ component, dependencies = [], valueField = undefined, labelField = undefined }: ListControllerParams) {
-    super({ component, dependencies, valueField, labelField } as ListControllerParams);
+    super({ component, dependencies, valueField, labelField });
     this.component = component;
     this._componentRoot = (this.component.shadowed) ? (this.component.shadowRoot) : (this.component.body);
 
--- src/ts/org.qcobjects.controllers.slider.ts
+++ src/ts/org.qcobjects.controllers.slider.ts
@@ -5,17 +5,8 @@
  * Author: Jean Machuca <correojean@gmail.com>
  *
  * Cross Browser Javascript Framework for MVC Patterns
- * QuickCorp/QCObjects is licensed under the
- * GNU Lesser General Public License v3.0
- * [LICENSE] (https://github.com/QuickCorp/QCObjects/blob/master/LICENSE.txt)
- *
- * Permissions of this copyleft license are conditioned on making available
- * complete source code of licensed works and modifications under the same
- * license or the GNU GPLv3. Copyright and license notices must be preserved.
- * Contributors provide an express grant of patent rights. However, a larger
- * work using the licensed work through interfaces provided by the licensed
- * work may be distributed under different terms and without source code for
- * the larger work.
+ * QCObjects is licensed under the MIT License
+ * [LICENSE] (https://github.com/QCObjects/qcobjects-sdk/blob/main/LICENSE.txt)
  *
  * Copyright (C) 2015 Jean Machuca,<correojean@gmail.com>
  *
@@ -43,7 +34,7 @@ export class SliderController extends Controller {
   component: Component;
 
   constructor({ component, dependencies = [], duration = 7100, slideIndex = 0, interval = null, sliderHandlerName = null }: SliderParams) {
-    super({ component, dependencies, duration, slideIndex, interval, sliderHandlerName } as SliderParams);
+    super({ component, dependencies, duration, slideIndex, interval, sliderHandlerName });
     this.component = component;
     this._componentRoot = (component.shadowed) ? (component.shadowRoot) : (component.body);
     this.sliderHandlerName = "slider_" + this.component.__instanceID.toString();
